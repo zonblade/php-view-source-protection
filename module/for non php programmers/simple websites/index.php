94da43a6077429ebc450c0036a761f09a2071e07
Agung Zon Blade <53517318+zonblade@users.noreply.github.com>
2020-12-04 07:47:40 -0800

    updating codes to v2.0.3 draft

--- module/for non php programmers/simple websites/index.php
+++ module/for non php programmers/simple websites/index.php
@@ -1,12 +1,32 @@
 <?php
-require __DIR__."/tools/viewsource_clean.php";
+require __DIR__."/viewsource_clean.php";
+/*
+kill unwanted cookies
+*/
 vsp_kill();
-vsp_inspect(true);
-/* input loader */
+/* inspect? */
+vsp_inspect(false);
+    /*
+input loader
+*/
 vsp_loader(__DIR__."/view/loader.html");
-/* page builder */
-vsp_builder('home',__DIR__."/view/home.html",true);
-vsp_builder('about',__DIR__."/view/about.html",true);
-vsp_builder('clean',__DIR__."/view/clean.html",false);
-vsp_default(true,"redirect","?home=");
+/*
+page builder
+vsp_builder('type'      ,'patameter' ,'path/url'                     ,true/false);
+*/
+vsp_builder('load'     ,'home'      ,__DIR__."/view/home.html"      ,true);
+vsp_builder('load'     ,'about'     ,__DIR__."/view/about.html"     ,true);
+vsp_builder('load'     ,'clean'     ,__DIR__."/view/clean.html"     ,false);
+/*
+redirect builer
+*/
+vsp_builder('redirect' ,'google'    ,'https://google.com'           ,false);
+/*
+vsp default tidak dapat dipakai berulang!
+jika digunakan secara berulang akan menimbulkan error!
+*/
+vsp_default(true        ,"redirect"  ,"home");
+/*
+kill again
+*/
 vsp_kill();
